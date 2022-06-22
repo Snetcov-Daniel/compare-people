a4f0f4d0559827b504246247baac8c6f0b13011d
NatanerJ <janovntn@gmail.com>
2022-06-22 22:28:59 +0300

    yes

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,10 +1,13 @@
 <template>
     <q-page class="main">
         <div class="compare flex justify-center">
-            <q-btn label="Compare" no-caps color="primary" class="compare__button"/>
+            <div  v-if="validated">
+              <router-link to='/compare'><q-btn label="Compare" no-caps color="primary" class="compare__button" /></router-link>
+            </div>
+            <h2 v-else>Please Choose One of all images</h2>
         </div>
         <div class="row">
-            <div v-for="index in 31" :key="index" class="col-4">
+            <div v-for="index in 31" :key="index" class="col-4" @click="handleClick($event)">
                 <img class="image" src="http://localhost:3000/api/v1/image" alt="" style="margin: 5px; height: 100%">
             </div>
         </div>
@@ -12,13 +15,15 @@
 </template>
 
 <script>
+import { BlockList } from "net";
 import getPeople from "src/API/request";
 
 export default {
     name: "Index",
     data() {
         return {
-            src: ['../../public/29.png']
+            src: ['../../public/29.png'],
+            validated: false,
         }
     },
     mounted() {
@@ -28,8 +33,13 @@ export default {
         // for (let i = 0; i < this.src.length; i++) {
         //     this.src[i] = require(`${this.src[i]}`)
         // }
-        console.log(this.src)
-    }
+    },
+    methods: {
+      handleClick (event) {
+        this.validated = !this.validated
+        this.validated === true ? event.target.style.border="10px solid #41B883" : event.target.style.border="1px solid black"
+      }
+    },
 }
 </script>
 
@@ -50,5 +60,10 @@ export default {
 
 .image {
     border: 1px solid black;
+}
+
+.a {
+  border: 10px solid #41B883;
+  box-sizing: border-box;
 }
 </style>
